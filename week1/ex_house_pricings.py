3a3ac220012b0e441a700e04f5e56993ce742c5e
Diego de los Reyes <diegorys@gmail.com>
2020-05-14 17:39:16 +0200

    Moved.

--- week1/ex_house_pricings.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow import keras
-model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
-xs = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype=float)
-ys = np.array([0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5], dtype=float)
-model.fit(xs, ys, epochs=1000)
-print(model.predict([7.0]))
