d8b0db3697d5e4a00c84bb2024c759e3851a1f88
Diego de los Reyes <diegorys@gmail.com>
2020-05-20 17:38:00 +0200

    Course 3, week1 finished.

---
+++ course3/week1/sarcasm.py
@@ -0,0 +1,28 @@
+# wget --no-check-certificate https://storage.googleapis.com/laurencemoroney-blog.appspot.com/sarcasm.json
+import json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+with open("../../datasets/sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+
+word_index = tokenizer.word_index
+print(len(word_index))
+print(word_index)
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post')
+print(sentences[2])
+print(padded[2])
+print(padded.shape)
